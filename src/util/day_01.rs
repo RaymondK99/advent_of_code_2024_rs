e7268487b8600a35cfb70ebee950f2b08413112d
Raymond Kalander <raymond.kalander@getswish.se>
2024-12-01 09:58:50 +0100

    day 1

--- src/util/day_01.rs
+++ src/util/day_01.rs
@@ -10,12 +10,37 @@ pub fn solve(input : String, part: Part) -> String {
     }
 }
 
-fn part1(_lines:Vec<&str>) -> String {
-    "1".to_string()
+fn parse(line:&str) -> (i32,i32) {
+    let numbers:Vec<&str> = line.split_ascii_whitespace().collect();
+    let left = numbers.first().unwrap().parse::<i32>().unwrap();
+    let right = numbers.last().unwrap().parse::<i32>().unwrap();
+    (left, right)
 }
 
-fn part2(_lines:Vec<&str>) -> String {
-    "2".to_string()
+fn part1(lines:Vec<&str>) -> String {
+    let mut first:Vec<i32> = lines.iter().map(|line| parse(line)).map(|item| item.0).collect();
+    let mut second:Vec<i32> = lines.iter().map(|line| parse(line)).map(|item| item.1).collect();
+    first.sort();
+    second.sort();
+
+    first.iter().enumerate()
+        .map(|(index, left)| left.abs_diff(second[index]))
+        .sum::<u32>()
+        .to_string()
+}
+
+fn part2(lines:Vec<&str>) -> String {
+    let first:Vec<usize> = lines.iter().map(|line| parse(line)).map(|item| item.0 as usize).collect();
+    let second:Vec<usize> = lines.iter().map(|line| parse(line)).map(|item| item.1 as usize).collect();
+    let mut freq_array = [0;100_000];
+
+    for index in second {
+        freq_array[index] += 1;
+    }
+
+    first.iter().map(|index| index * freq_array[*index])
+        .sum::<usize>()
+        .to_string()
 }
 
 
@@ -30,26 +55,36 @@ mod tests {
     #[test]
     fn test1() {
 
-        let input = "";
-        assert_eq!("1", solve(input.to_string(), Part1));
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("11", solve(input.to_string(), Part1));
     }
 
     #[test]
     fn test_part1() {
         let input = include_str!("../../input/input_01.txt");
-        assert_eq!("1", solve(input.to_string(), Part1));
+        assert_eq!("2344935", solve(input.to_string(), Part1));
     }
 
     #[test]
     fn test2() {
 
-        let input = "";
-        assert_eq!("2", solve(input.to_string(), Part2));
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("31", solve(input.to_string(), Part2));
     }
 
     #[test]
     fn test_part2() {
         let input = include_str!("../../input/input_01.txt");
-        assert_eq!("2", solve(input.to_string(), Part2));
+        assert_eq!("27647262", solve(input.to_string(), Part2));
     }
 }
