16d4e8af5b6b00ec835ba7149e1af696c5f4386f
Raymond Kalander <raymond.kalander@getswish.se>
2024-12-02 09:22:06 +0100

    added day 2

--- src/util/day_02.rs
+++ src/util/day_02.rs
@@ -10,12 +10,57 @@ pub fn solve(input : String, part: Part) -> String {
     }
 }
 
-fn part1(_lines:Vec<&str>) -> String {
-    "1".to_string()
+
+fn is_safe(levels:&Vec<u32>) -> bool {
+    let increasing = levels[0] < levels[1];
+    for i in 1..levels.len() {
+        let last = levels[i-1];
+        let next = levels[i];
+        let next_increasing = next > last;
+        let delta = next.abs_diff(last);
+        if delta == 0 || delta > 3 || next_increasing != increasing {
+            return false;
+        }
+    }
+
+    true
 }
 
-fn part2(_lines:Vec<&str>) -> String {
-    "2".to_string()
+
+fn parse(lines:Vec<&str>) -> Vec<Vec<u32>> {
+    let mut list = vec![];
+    for line in lines {
+        let levels = line.split_ascii_whitespace().into_iter().map(|item| item.parse().unwrap()).collect();
+        list.push(levels);
+    }
+    list
+}
+
+fn part1(lines:Vec<&str>) -> String {
+    let list = parse(lines);
+    list.iter().filter(|levels| is_safe(levels)).count().to_string()
+}
+
+fn part2(lines:Vec<&str>) -> String {
+    let list = parse(lines);
+    let mut sum = 0;
+    for mut numbers in list {
+        if is_safe(&numbers) {
+            sum += 1;
+        } else {
+            for i in 0..numbers.len() {
+                let removed = numbers.remove(i);
+                let is_safe = is_safe(&numbers);
+                numbers.insert(i, removed);
+                if is_safe {
+                    sum += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    sum.to_string()
 }
 
 
@@ -30,26 +75,37 @@ mod tests {
     #[test]
     fn test1() {
 
-        let input = "";
-        assert_eq!("1", solve(input.to_string(), Part1));
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!("2", solve(input.to_string(), Part1));
     }
 
     #[test]
     fn test_part1() {
         let input = include_str!("../../input/input_02.txt");
-        assert_eq!("1", solve(input.to_string(), Part1));
+        assert_eq!("236", solve(input.to_string(), Part1));
     }
 
     #[test]
     fn test2() {
 
-        let input = "";
-        assert_eq!("2", solve(input.to_string(), Part2));
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!("4", solve(input.to_string(), Part2));
     }
 
     #[test]
     fn test_part2() {
+        // 291 => too low
         let input = include_str!("../../input/input_02.txt");
-        assert_eq!("2", solve(input.to_string(), Part2));
+        assert_eq!("308", solve(input.to_string(), Part2));
     }
 }
