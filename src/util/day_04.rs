c9f846e7bd335291eec83f830134d056f502bf86
Raymond Kalander <raymond.kalander@getswish.se>
2024-12-04 10:22:51 +0100

    added 4b

--- src/util/day_04.rs
+++ src/util/day_04.rs
@@ -29,6 +29,30 @@ fn find_xmas(map:&Vec<Vec<u8>>, x0:i32, y0:i32, dx:i32, dy:i32) -> bool {
     true
 }
 
+fn get_char(map:&Vec<Vec<u8>>, x:i32, y:i32) -> char {
+    if y < 0 || x < 0 || y as usize >= map.len() || x as usize >= map[0].len() {
+        return '#';
+    } else {
+        return map[y as usize][x as usize] as char;
+    }
+}
+
+fn find_mas(map:&Vec<Vec<u8>>, x:i32, y:i32) -> bool {
+    if map[y as usize][x as usize] == 'A' as u8 {
+        let up_left: char = get_char(map, x-1, y-1);
+        let down_left: char = get_char(map, x-1, y+1);
+        let up_right: char = get_char(map, x+1, y-1);
+        let down_right: char = get_char(map, x+1, y+1);
+
+        if up_left == 'M' && down_right == 'S' || up_left == 'S' && down_right == 'M' {
+            if up_right == 'M' && down_left == 'S' || up_right == 'S' && down_left == 'M' {
+                return true;
+            }
+        }
+    }
+    false
+}
+
 
 fn part1(lines:Vec<&str>) -> String {
     let map = get_map(&lines);
@@ -46,8 +70,19 @@ fn part1(lines:Vec<&str>) -> String {
     result.to_string()
 }
 
-fn part2(_lines:Vec<&str>) -> String {
-    "2".to_string()
+fn part2(lines:Vec<&str>) -> String {
+    let map = get_map(&lines);
+    let mut result = 0;
+
+    for y in 0..map.len() as i32 {
+        for x in 0..map[0].len() as i32 {
+            if find_mas(&map, x, y) {
+                result += 1;
+            }
+        }
+    }
+
+    result.to_string()
 }
 
 
@@ -84,13 +119,22 @@ S.S.S.S.SS
     #[test]
     fn test2() {
 
-        let input = "";
-        assert_eq!("2", solve(input.to_string(), Part2));
+        let input = ".M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........";
+        assert_eq!("9", solve(input.to_string(), Part2));
     }
 
     #[test]
     fn test_part2() {
         let input = include_str!("../../input/input_04.txt");
-        assert_eq!("2", solve(input.to_string(), Part2));
+        assert_eq!("1925", solve(input.to_string(), Part2));
     }
 }
